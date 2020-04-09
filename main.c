5a6e07023a895ce6930efa26c4d00e7e8da237a2
Suguivy <adrisolgo7373@gmail.com>
2020-04-09 19:28:25 +0200

    Prevent stdin redirection from files or pipe

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include "editor.h"
+#include "util.h"
 
 int main() {
-  struct editor *ed = editor_new();
+  if (STDIN_IS_TERMINAL()) { // To prevent input of pipes and files
+    struct editor *ed = editor_new();
   
-  while(!editor_needs_exit(ed)) {
-    editor_interpret_key(ed, editor_get_key());
-    editor_refresh(ed);
+    while(!editor_needs_exit(ed)) {
+      editor_interpret_key(ed, editor_get_key());
+      editor_refresh(ed);
+    }
+    
+    editor_exit(ed);
+    return 0;
+  } else {
+    fprintf(stderr, "ERROR: The input is not a terminal\n");
+    return -1;
   }
-
-  editor_exit(ed);
-  
-  return 0;
 }
--- util.h
+++ util.h
@@ -1,7 +1,11 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <unistd.h>
+#include <stdio.h>
 #include <stddef.h>
+
+#define STDIN_IS_TERMINAL() (isatty(fileno(stdin)))
 
 void *safe_malloc(size_t n);
 
